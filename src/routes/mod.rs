b4a40dc5e25fdbb9c19caebbc9a0e98a1bb8f310
JiMarchel <jmarchel100@gmail.com>
2024-09-27 08:30:44 +0700

    middleware

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,8 @@
 mod body_json;
 mod body_string;
 mod hello;
+mod middleware_custom_header;
+mod middleware_message;
 mod path_variables;
 mod query_params;
 
@@ -10,23 +12,37 @@ use hello::hello_wib;
 
 use axum::{
     routing::{get, post},
-    Router,
+    Extension, Router,
 };
 use hyper::Method;
+use middleware_custom_header::middleware_custom_header;
+use middleware_message::middleware_message;
 use path_variables::path_variables;
 use query_params::query_params;
 use tower_http::cors::{Any, CorsLayer};
+
+#[derive(Clone)]
+pub struct SharedData {
+    pub message: String,
+}
 
 pub fn create_routes() -> Router {
     let cors = CorsLayer::new()
         .allow_methods([Method::GET])
         .allow_origin(Any);
 
+    let shared_data = SharedData {
+        message: "Hello from middleware".to_owned(),
+    };
+
     axum::Router::new()
         .route("/", get(hello_wib))
         .route("/user/:user_id/team/:team_id", get(path_variables))
         .route("/query_params", get(query_params))
+        .route("/middleware_message", get(middleware_message))
         .route("/body_string", post(body_str))
         .route("/body_json", post(body_json))
         .layer(cors)
+        .layer(Extension(shared_data))
+        .route("/middleware_custom_header", get(middleware_custom_header))
 }
